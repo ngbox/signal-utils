100c76bf3d8e702c270ae7d649b657e6d6586678
nevzatopcu <nevzatopcu@gmail.com>
2023-09-06 13:59:52 +0300

    from-async introduced

---
+++ packages/signal-utils/src/lib/from-async.ts
@@ -0,0 +1,63 @@
+import {
+  CreateEffectOptions,
+  CreateSignalOptions,
+  DestroyRef,
+  Injector,
+  Signal,
+  assertInInjectionContext,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  Observable,
+  ReplaySubject,
+  catchError,
+  takeUntil,
+  throwError,
+} from 'rxjs';
+
+export type FromAsyncOptions<T> = CreateSignalOptions<T> &
+  CreateEffectOptions & { initialValue: T | null };
+
+export function fromAsync<T, K>(
+  observable$: Observable<T>,
+  options: FromAsyncOptions<T>
+): {
+  data: Signal<T | null>;
+  loading: Signal<boolean>;
+  error: Signal<K | null>;
+} {
+  options.injector ?? assertInInjectionContext(fromAsync);
+  const injector = options.injector ?? inject(Injector);
+  const destroyRef = injector.get(DestroyRef);
+
+  const destroyer$ = new ReplaySubject<void>(1);
+  destroyRef.onDestroy(() => destroyer$.next());
+
+  const loading = signal(false);
+  const data = signal<T | null>(options.initialValue);
+  const error = signal<K | null>(null);
+
+  const startFetching = () => {
+    observable$
+      .pipe(
+        catchError((err) => {
+          error.set(err);
+          return throwError(() => err);
+        }),
+        takeUntil(destroyer$)
+      )
+      .subscribe((resp) => {
+        data.set(resp);
+        loading.set(false);
+      });
+  };
+
+  startFetching();
+
+  return {
+    loading,
+    data,
+    error,
+  };
+}
