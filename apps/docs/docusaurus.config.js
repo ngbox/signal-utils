d082fecf2cf92618594d6b9b9dc330e08643f61e
nevzatopcu <nevzatopcu@gmail.com>
2023-09-16 21:39:08 +0300

    docs: light mode disabled

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -111,8 +111,8 @@ const config = {
       },
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: false,
-        respectPrefersColorScheme: true,
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
       },
     }),
 };
