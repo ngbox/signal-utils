84e66f8fdb26d0837db06c6d9fbb995c3dabb662
nevzatopcu <nevzatopcu@gmail.com>
2023-09-21 19:14:17 +0300

    trigger pipeline

--- apps/docs/sidebars.js
+++ apps/docs/sidebars.js
@@ -2,7 +2,7 @@
  * Creating a sidebar enables you to:
  - create an ordered group of docs
  - render a sidebar for each doc of that group
- - provide next/previous navigation
+ - provide next/previous navigation 
 
  The sidebars can be generated from the filesystem, or explicitly defined here.
 
